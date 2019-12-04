a8c8c55fc6e94571617337cf42357bc52a9d1c63
admin <admin@TeamCity>
2019-12-04 23:32:05 +0300

    TeamCity change in 'cleanup1 subproject дщтп тфьу ещ срусл' project: Cleanup rule was deleted for build type

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -78,12 +78,6 @@ object CleanConfig : BuildType({
 
     cleanup {
         keepRule {
-            id = "KEEP_RULE_4"
-            keepAtLeast = allBuilds()
-            dataToKeep = everything()
-            preserveArtifactsDependencies = true
-        }
-        keepRule {
             id = "KEEP_RULE_5"
             keepAtLeast = allBuilds()
             dataToKeep = everything()
